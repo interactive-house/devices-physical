25d01524a484d6415a4b56ca4dd2dfd598377d00
Sandra <79096992+average-bot@users.noreply.github.com>
2023-03-20 15:28:13 +0100

    Arduino change according to db & the other way around
    Grab updates for light, door and window.
    Update db with soil status from the arduino.

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import serial
 import firebase_admin
-import time
-import datetime
-import ast
 from firebase_admin import db
 from firebase_admin import credentials
 
 url = "https://soft-d20bd-default-rtdb.firebaseio.com/";
-cred = credentials.Certificate('soft-d20bd-firebase-adminsdk-ho640-8bafc4b748.json')
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('soft-d20bd-firebase-adminsdk-ho640-c6075710e1.json')
+
+# Initialize the app with a service account, granting admin privileges
 default_app = firebase_admin.initialize_app(cred, {'databaseURL': url})
-ref = db.reference('/heads')
-print("Hello")
-arduino = serial.Serial('COM5', 9600, timeout=.1)
-print("Hello")
 
-def toFirebase(soilInfo):
-    print(soilInfo)
-    timeNow = datetime.datetime.now().time()
-    ref.child('0').push({ "soil":soilInfo})
-    ref.child('0').update({'soils':{"soil":soilInfo}})
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference("Data")
 
+windows = ref.child("window")
+door = ref.child("door")
+lamp = ref.child("light")
+soil = ref.child("soil")
+
+arduino = serial.Serial('COM3', 9600, timeout=.1)
+
+isWindowOpen = False
+isDoorOpen = False
+isLampOn = False
 while True:
-    data = arduino.readline()[:-2].decode("utf-8")
-    if data!="":
-        state = str(ast.literal_eval(data)['soil'])
-        toFirebase()
+    
+    windowFromFireBase = windows.get()
+    doorFromFireBase = door.get()
+    lampFromFireBase = lamp.get()
 
+    if(isWindowOpen != windowFromFireBase):
+        arduino.write(b'w')
+        isWindowOpen= not isWindowOpen
+    
+
+    if(isDoorOpen != doorFromFireBase):
+        arduino.write(b'd')
+        isDoorOpen= not isDoorOpen
+    
+
+    if(isLampOn != lampFromFireBase):
+        arduino.write(b'l')
+        isLampOn= not isLampOn
+
+    dataFromArduino = arduino.readline().decode().strip()
+    if('Soil' in dataFromArduino):
+        split = dataFromArduino.split(': ')
+        soil.set(split[1])
+
+    
