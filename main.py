b0eb92b819476765236f06f3d7d6bb178caee54a
Sandra <79096992+average-bot@users.noreply.github.com>
2023-05-25 10:15:29 +0200

    startup logic added (final presentation code)

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import serial
 import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
+import time
 
 url = "https://smarthome-3bb7b-default-rtdb.firebaseio.com/"
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate(
-    "smarthome-3bb7b-firebase-adminsdk-bv9hc-17639baa20.json"
+    "smarthome-3bb7b-firebase-adminsdk-bv9hc-de16c406f8.json"
 )
 
 # Initialize the app with a service account, granting admin privileges
@@ -20,13 +21,21 @@ windows = ref.child("SmartHomeValueWindow").child("StatusOfWindow")
 door = ref.child("SmartHomeValueDoor").child("StatusOfDoor")
 lamp = ref.child("SmartHomeValueLight").child("StatusOflight")
 soil = ref.child("SmartHomeValueSoil").child("StatusOfSoil")
-arduino = serial.Serial("COM3", 9600, timeout=0.1)
+arduino = serial.Serial("COM5", 9600, timeout=0.1)
 
-windowStatus = "closed"
-doorStatus = "closed"
-lampStatus = "off"
+time.sleep(5)
+# To check if the modules are on/off at the start
+windowStatus = windows.get()
+doorStatus = door.get()
+lampStatus = lamp.get()
+if windowStatus == 'open':
+    arduino.write(b"w")
+if doorStatus == 'open':
+    arduino.write(b"d")
+if lampStatus == 'on':
+    arduino.write(b"l")
+
 while True:
-
     windowFromFireBase = windows.get()
     doorFromFireBase = door.get()
     lampFromFireBase = lamp.get()
